89136f1f516a1a06257871fa0d661212be82e679
mgameiro <miguelgameiro94@hotmail.com>
2016-11-05 14:01:51 +0000

    Código básico para classe InfreRedLED

--- mailNotifier/InfraRedLED.cpp
+++ mailNotifier/InfraRedLED.cpp
@@ -1,1 +1,39 @@
 #include "InfraRedLED.h"
+
+
+InfraRedLED::InfraRedLED(int NLED){
+  int n;
+  
+  this->NLED = NLED;
+  for(n=0; n<NLED; n++){
+    pinMode(this->LEDPin[n], OUTPUT);
+  }
+}
+
+void InfraRedLED::turnLEDON(int LEDPin){
+  digitalWrite(LEDPin, HIGH);
+}
+
+void InfraRedLED::turnAllON(){
+  int n;
+  // Acende todos os LEDs
+  for(n=0; n<NLED; n++){
+    turnLEDON(this->LEDPin[n]);
+  }
+}
+
+void InfraRedLED::turnLEDOFF(int LEDPin){
+  digitalWrite(LEDPin, LOW);
+}
+
+void InfraRedLED::turnAllOFF(){
+  int n;
+  // Acende todos os LEDs
+  for(n=0; n<NLED; n++){
+    turnLEDOFF(this->LEDPin[n]);
+  }
+}
+
+InfraRedLED::~InfraRedLED(){
+  
+}
--- mailNotifier/InfraRedLED.h
+++ mailNotifier/InfraRedLED.h
@@ -1,8 +1,19 @@
 #ifndef INFRAREDLED_H_INCLUDED
 #define INFRAREDLED_H_INCLUDED
 
+#include "Arduino.h"
+
 class InfraRedLED{
-  
+  int NLED;
+  int LEDPin[3];
+
+public:
+  InfraRedLED(int NLED);
+  void turnLEDON(int LEDPin);
+  void turnAllON();
+  void turnLEDOFF(int LEDPin);
+  void turnAllOFF();
+  ~InfraRedLED();
 };
 
 #endif
